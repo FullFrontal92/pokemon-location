cfca4a72b84ceeb8a5bcb66108371584dfc5e0a8
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-04-24 00:22:24 +0200

    the core is here, few change to do

---
+++ app/src/main/java/com/example/adambenyahia/location/MyLocationListener.java
@@ -0,0 +1,44 @@
+package com.example.adambenyahia.location;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by adambenyahia on 20/03/2018.
+ */
+
+public class MyLocationListener implements LocationListener {
+    Context context;
+    public static Location location;
+    public MyLocationListener(Context context){
+        this.context  =context ;
+        location= new Location("Start");
+        location.setLatitude(0);
+        location.setLongitude(0);
+
+    }
+    public void onLocationChanged(Location location) {
+        this.location=location;
+    }
+
+    public void onStatusChanged(String s, int i, Bundle b) {
+        //  Toast.makeText(context, "Provider status changed",
+        //         Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderDisabled(String s) {
+        Toast.makeText(context,
+                "  GPS turned off . you cannot follow your locations",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onProviderEnabled(String s) {
+        // SettingSaved.LoadData(context);
+        Toast.makeText(context,
+                " GPS turned on. you can follow your locations",
+                Toast.LENGTH_LONG).show();
+    }
+}
